107c9dad7d2d0fb6a0de9ca6ec0d1290eb79f1c0
Ephemeral25 <clemdem61@gmail.com>
2020-02-28 15:36:42 +0100

    Added Login with Express backend using sqlite3

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,50 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+      <router-link
+        v-if="authenticated"
+        to="/login"
+        v-on:click.native="logout()"
+        replace
+        >Logout</router-link
+      >
     </div>
-    <img src="./assets/logo-car.png">
-    <router-view @authenticated="setAuthenticated" />
+    <img src="./assets/logo-car.png" />
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       authenticated: false,
       devAccount: {
         username: "test",
         password: "test"
-    }
-    }
+      }
+    };
   },
   mounted() {
-    if(!this.authenticated) {
+    /* if (!this.authenticated) {
       this.$router.replace({ name: "login" });
-    }
+    }*/
   },
   methods: {
-    setAuthenticated(status) {
+    /*  setAuthenticated(status) {
       this.authenticated = status;
-    },
+    },*/
     logout() {
       this.authenticated = false;
     }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
