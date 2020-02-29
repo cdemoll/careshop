97183ff30e72c49eecc6455cf32cdd1c19b04a23
goulou toure <gouloudu78@gmail.com>
2020-02-29 11:31:05 +0100

    Addition of Navigation BAR

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-<<<<<<< HEAD
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Register from './components/Register'
-=======
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import App from "./App";
 import router from "./router/index.js";
 import Axios from "axios";
->>>>>>> 69fb609d98f2104b36ff9b33bcc5d26a587d2910
+
 
 Vue.prototype.$http = Axios;
 Vue.use(Vuetify);
