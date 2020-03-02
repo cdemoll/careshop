a94083e1061c43eff7a9d9c4d85b5dac9e9bda00
goulou toure <gouloudu78@gmail.com>
2020-03-02 13:39:16 +0100

    Nav bar fix

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,50 +1,26 @@
+
 <template>
-
-
-<v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height brown lighten-5" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card class="elevation-12">
-              <v-toolbar dark flat>
-                <v-toolbar-tittle>Login</v-toolbar-tittle>
-              </v-toolbar>
-
-
-
   <div id="login">
-    
+    <h1>Login</h1>
     <form>
       <label for="email">E-Mail Address</label>
-      
       <div>
-          <v-text-field>
         <input id="email" type="email" v-model="email" required autofocus />
-        </v-text-field>
       </div>
       <div>
         <label for="password">Password</label>
         <div>
-           <v-text-field>
           <input id="password" type="password" v-model="password" required />
-          </v-text-field>
         </div>
       </div>
       <div>
-        <button type="submit" @click="handleSubmit">
-          Login
-        </button>
+        <button type="submit" @click="handleSubmit">Login</button>
       </div>
     </form>
-   </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-</v-app>
+    <v-btn v-if="authenticated" to="/login" @click="logout()">Logout</v-btn>
+  </div>
 </template>
+
 
 <script>
 export default {
